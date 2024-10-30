829119f0f8d5c9c416cb1128e89bd4f6f5d9bd7e
sunil dhayal <sunildhayal368@gmail.com>
2024-10-30 21:38:02 +0530

    practices javascript

---
+++ 29_10.js
@@ -0,0 +1,53 @@
+
+
+let arr = ["sunil" , 23 , 35.5];
+
+arr.unshift("amit");
+
+console.log(arr);
+
+
+arr.shift();
+
+console.log(arr);
+
+arr.push("rahul");
+
+console.log(arr);
+
+arr.pop(arr);
+console.log(arr);
+
+let primary = [ "sunil " , "rahul" , "amit" , "anil"];
+
+let secondary = ["vikas" , "sonu" , "rohit"];
+
+console.log(primary.concat(secondary));
+
+
+console.log(primary);
+
+console.log(secondary);
+
+primary.reverse();
+
+console.log(primary);
+
+primary.slice(1,2);
+
+console.log(primary);
+
+primary.splice(1,2);
+
+console.log(primary);
+
+primary.sort();
+
+console.log(primary);
+
+
+let days  = ["sumit","sukh","amit","anil"];
+
+console.log(days.sort());
+
+console.log(days);
