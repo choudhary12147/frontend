f4d96ac5e71fa15172fb75a9d71132a41d4bdf1c
sunil dhayal <sunildhayal368@gmail.com>
2024-11-06 22:54:15 +0530

    done javascript

--- 29_10.js
+++ 29_10.js
@@ -4,38 +4,38 @@ let arr = ["sunil" , 23 , 35.5];
 
 arr.unshift("amit");
 
-console.log(arr);
+// console.log(arr);
 
 
 arr.shift();
 
-console.log(arr);
+// console.log(arr);
 
 arr.push("rahul");
 
-console.log(arr);
+// console.log(arr);
 
 arr.pop(arr);
-console.log(arr);
+// console.log(arr);
 
 let primary = [ "sunil " , "rahul" , "amit" , "anil"];
 
 let secondary = ["vikas" , "sonu" , "rohit"];
 
-console.log(primary.concat(secondary));
+// console.log(primary.concat(secondary));
 
 
-console.log(primary);
+// console.log(primary);
 
-console.log(secondary);
+// console.log(secondary);
 
-primary.reverse();
+// primary.reverse();
 
-console.log(primary);
+// console.log(primary);
 
-primary.slice(1,2);
+// primary.slice(1,2);
 
-console.log(primary);
+// console.log(primary);
 
 primary.splice(1,2);
 
