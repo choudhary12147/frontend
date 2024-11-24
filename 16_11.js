567396d871deff2d156a524ed12f4f1a82e5f5f5
sunil dhayal <sunildhayal368@gmail.com>
2024-11-24 11:51:43 +0530

    done the assingment

--- 16_11.js
+++ 16_11.js
@@ -4,3 +4,18 @@ btn.onclick = function () {
     console.log("button was clicked");
 };
 
+btn.onclick = clicked;
+
+function clicked() {
+     console.log("clicked");
+}
+
+
+// event listener
+
+
+let btn1 = document.getElementById("btn1");
+
+btn1.addEventListener("click",function () {
+    console.log("new btn was clicked");
+});
